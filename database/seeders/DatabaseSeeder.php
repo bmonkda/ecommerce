d1d2e9eb84dd479026ed3499664824e74473a070
Benjamín <bmonkdareg@gmail.com>
2026-04-17 21:08:38 -0400

    Introduciendo datos de prueba para productos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,14 +17,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Storage::deleteDirectory('products');
+        Storage::makeDirectory('products');
+        
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Benjamín',
+            'name' => 'Benjamín Moncada',
             'email' => 'test@example.com',
         ]);
 
-
         $this->call([FamilySeeder::class]);
+
+        Product::factory(150)->create();
     }
 }
