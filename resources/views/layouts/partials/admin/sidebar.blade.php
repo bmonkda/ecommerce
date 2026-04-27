7b4aa9c00205bdc65893a3bbddbf07c9c4d542b4
Benjamín <bmonkdareg@gmail.com>
2026-04-26 23:41:29 -0400

    Listar opciones

--- resources/views/layouts/partials/admin/sidebar.blade.php
+++ resources/views/layouts/partials/admin/sidebar.blade.php
@@ -7,6 +7,12 @@
             'active' => request()->routeIs('admin.dashboard')
         ],
         [
+            'icon' => 'fa-solid fa-cog',
+            'name' => 'Opciones',
+            'route' => route('admin.options.index'),
+            'active' => request()->routeIs('admin.optiones.index')
+        ],
+        [
             // familias de productos
             'name' => 'Familias',
             'icon' => 'fa-solid fa-box-open',
